b7c722b62aea4f3f981276c4e9af3dd810889646
Erick Hartawan <ehartawan@lumination.com.au>
2022-12-08 17:12:52 +1030

    added variable port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { default: axios } = require('axios')
 const express = require('express')
 const helmet = require('helmet')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(helmet())
 
